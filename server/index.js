f74a88b1696f5fe145b6357bdbc5561ad6979fcf
Marquis <theusual@icloud.com>
2019-11-07 13:24:17 -0700

    initial

---
+++ server/index.js
@@ -0,0 +1,47 @@
+// Import config
+const keys = require('./keys');
+
+// Express app setup
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+// MongoDB Client setup
+const mongoUrl = `mongodb://${keys.mongoUser}:${keys.mongoPass}@${keys.mongoHost}:${keys.mongoPort}/${keys.dbName}`;
+const MongoClient = require('mongodb').MongoClient;
+
+// Express route handlers
+app.get('/', (req, res) => {
+  res.send('Lib or Builder');
+});
+
+app.get('/library', async (req, res) => {
+  let result = await MongoClient.connect(mongoUrl, function(err, db) {
+    if (err) res.send(err);
+    var dbo = db.db(keys.dbName);
+    dbo.collection("components").findOne({}, function(err, result) {
+      if (err) throw err;
+      res.send(result);
+    });
+  });
+});
+
+app.get('/builder', (req, res) => {
+  res.send(mongoUrl);
+});
+
+app.post('/builder', async (req, res) => {
+  const tags = req.body.tags;
+  const name = req.body.name;
+  const type = req.body.type;
+  const styles = req.body.styles;
+  const markup = req.body.markup;
+
+  res.send({working:true});
+});
+
+app.listen(5000, err => {
+  console.log('listening on port 5000');
+});
