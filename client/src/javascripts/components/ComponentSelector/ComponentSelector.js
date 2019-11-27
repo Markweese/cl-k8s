107df34191f133548d0be37de5a6b30e0ede4114
Marquis <theusual@icloud.com>
2019-11-26 19:27:16 -0700

    resolve breaking react test

---
+++ client/src/javascripts/components/ComponentSelector/ComponentSelector.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import axios from 'axios';
+
+class ComponentSelector extends Component {
+  state = {
+  };
+
+  componentDidMount() {
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>This will be the component selector</h2>
+      </div>
+    )
+  }
+}
+
+export default ComponentSelector;
