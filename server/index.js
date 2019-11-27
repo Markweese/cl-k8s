f154036d89aedb79f6e81be5d8141f01bbef02b2
Marquis <theusual@icloud.com>
2019-11-26 20:15:53 -0700

    add nginx and dockerfile.dev for local development, update cert manager deployments

--- server/index.js
+++ server/index.js
@@ -9,37 +9,62 @@ const app = express();
 app.use(bodyParser.json());
 
 // MongoDB Client setup
-const mongoUrl = `mongodb://${keys.mongoUser}:${keys.mongoPass}@${keys.mongoHost}:${keys.mongoPort}/${keys.dbName}`;
+const mongoUrl = `mongodb://${keys.mongoUser}:${keys.mongoPass}@${keys.mongoHost}.mlab.com:${keys.mongoPort}/${keys.dbName}`;
 const MongoClient = require('mongodb').MongoClient;
 
 // Express route handlers
 app.get('/', (req, res) => {
+  console.log(mongoUrl);
   res.send('Lib or Builder');
 });
 
-app.get('/library', async (req, res) => {
+app.get('/components', async (req, res) => {
   let result = await MongoClient.connect(mongoUrl, function(err, db) {
     if (err) res.send(err);
     var dbo = db.db(keys.dbName);
-    dbo.collection("components").findOne({}, function(err, result) {
+    dbo.collection("components").find({}).toArray(function(err, result) {
       if (err) throw err;
       res.send(result);
     });
   });
 });
 
-app.get('/builder', (req, res) => {
-  res.send(mongoUrl);
+app.get('/tags', async (req, res) => {
+  let result = await MongoClient.connect(mongoUrl, { useNewUrlParser: true }, function(err, db) {
+    if (err) res.send(err);
+    var dbo = db.db(keys.dbName);
+    dbo.collection("components").find({}).project({_id:0, tags:1}).toArray(function(err, result) {
+      if(err) throw err;
+      res.send(result);
+    });
+  });
 });
 
-app.post('/builder', async (req, res) => {
+app.get('/types', async (req, res) => {
+  let result = await MongoClient.connect(mongoUrl, { useNewUrlParser: true }, function(err, db) {
+    if (err) res.send(err);
+    var dbo = db.db(keys.dbName);
+    dbo.collection("components").find({}).project({_id:0, type:1}).toArray(function(err, result) {
+      if(err) throw err;
+      res.send(result);
+    });
+  });
+});
+
+app.post('/components', async (req, res) => {
   const tags = req.body.tags;
   const name = req.body.name;
   const type = req.body.type;
-  const styles = req.body.styles;
   const markup = req.body.markup;
 
-  res.send({working:true});
+  let result = await MongoClient.connect(mongoUrl, { useNewUrlParser: true }, function(err, db) {
+    if (err) res.send(err);
+    var dbo = db.db(keys.dbName);
+    dbo.collection("components").insertOne({name, type, tags, markup}, function(err, result) {
+      if(err) throw err;
+      res.send({ success:1 });
+    });
+  });
 });
 
 app.listen(5000, err => {
